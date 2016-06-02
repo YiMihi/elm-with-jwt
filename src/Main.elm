5059d150d386298e16728ccd5a3ef1ab4ad76997
Kim Maida <git@kim-maida.com>
2016-06-02 00:16:00 -0400

    * Add dependencies (html, http). * Create a simple view with two buttons and a model that is updated by clicking the buttons.

---
+++ src/Main.elm
@@ -0,0 +1,37 @@
+import Html exposing (Html, button, div, text)
+import Html.App as Html
+import Html.Events exposing (onClick)
+
+main = 
+    Html.beginnerProgram { model = model, view = view, update = update }
+    
+-- Model
+
+type alias Model = String
+
+model : Model
+model =
+    "Some quote"
+    
+-- Update
+
+type Msg = AddA | AddB
+
+update : Msg -> Model -> Model  
+update msg model =
+    case msg of
+        AddA ->
+            model ++ " A"
+            
+        AddB ->
+            model ++ " B"
+            
+-- View
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick AddA ] [ text "Add an A" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick AddB ] [ text "Add a B" ]
+        ]            
