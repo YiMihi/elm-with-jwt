60cf38b67164e0007bea56b566c81b925eefbce6
Kim Maida <git@kim-maida.com>
2016-06-03 13:40:07 -0400

    * Getting random quotes from API successfully.

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,7 @@ import Html.App as Html
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Http
-import Json.Decode as Json exposing ((:=))
+import Json.Decode as Decode
 import Task
 
 main : Program Never
@@ -19,25 +19,19 @@ main =
     
 -- Model
 
-type alias Model =
-    { quote : String }
+type alias Model = String
     
 init : (Model, Cmd Msg)
 init =
-    (Model "", Cmd.none)
+    ("", Cmd.none)
     
--- Update
+-- Messages
 
 type Msg 
     = GetQuote
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        GetQuote ->
-            (model, Cmd.none)
-            
-            
+    | FetchSuccess String
+    | FetchError Http.Error    
+               
 -- View
 
 view : Model -> Html Msg
@@ -45,17 +39,35 @@ view model =
     div [ class "jumbotron row text-center" ] [
         h2 [] [ text "Chuck Norris Quoter" ]
         , button [ class "btn btn-primary", onClick GetQuote ] [ text "Get a Quote" ]
-        , blockquote [ ] [ text (model.quote) ]
+        , blockquote [ ] [ text model ]
     ]
+    
+url : String
+url =    
+    "http://localhost:3001/api/random-quote"
+    
+fetchTask : Platform.Task Http.Error String
+fetchTask =
+    Http.getString url
+    
+fetchCmd : Cmd Msg
+fetchCmd =
+    Task.perform FetchError FetchSuccess fetchTask     
+    
+-- Update
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        GetQuote ->
+            (model, fetchCmd)
+        FetchSuccess quote ->
+            (quote, Cmd.none)
+        FetchError _ ->
+            (model, Cmd.none)       
     
 -- Subscriptions
 
 subscriptions : Model -> Sub Msg
 subscriptions model = 
     Sub.none
-    
--- HTTP
-
-getRandomQuote : Platform.Task Http.Error String
-getRandomQuote = 
-    Http.getString "http://localhost:3001/api/random-quote"
