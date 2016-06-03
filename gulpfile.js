ff89abb4ce6636b5528b179b7259c3f4fdf3ec8a
Kim Maida <git@kim-maida.com>
2016-06-03 12:47:19 -0400

    * Create index file and use stylesheets; pull in Bootstrap. Clean up steps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var elm = require('gulp-elm');
 var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var connect = require('gulp-connect');
 
 // File paths
 var paths = {
   dest: 'dist',
-  elm: 'src/*.elm' 
+  elm: 'src/*.elm',
+  static: 'src/*.{html,css}'
 };
 
 // Init Elm
@@ -15,7 +17,15 @@ gulp.task('elm-init', elm.init);
 // Compile Elm to HTML
 gulp.task('elm', ['elm-init'], function(){
     return gulp.src(paths.elm)
-        .pipe(elm({ filetype: 'html'})).on('error', errorHandler)
+        .pipe(plumber())
+        .pipe(elm()).on('error', errorHandler)
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Move static assets to dist
+gulp.task('static', function() {
+    return gulp.src(paths.static)
+        .pipe(plumber())
         .pipe(gulp.dest(paths.dest));
 });
 
@@ -26,9 +36,10 @@ gulp.task('elm-bundle', ['elm-init'], function(){
         .pipe(gulp.dest('dist/'));
 });
 
-// Watch for changes and compile Elm
+// Watch for changes and compile
 gulp.task('watch', function() {
     gulp.watch(paths.elm, ['elm']);
+    gulp.watch(paths.static, ['static']);
 });
 
 // Local server
@@ -47,5 +58,5 @@ function errorHandler(err){
 }
 
 // Main gulp tasks
-gulp.task('build', ['elm']);
+gulp.task('build', ['elm', 'static']);
 gulp.task('default', ['connect', 'build', 'watch']);
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as Html
 import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 import Http
-import Json.Decode exposing (list, string)
+import Json.Decode as Json exposing ((:=))
 import Task
 
 main : Program Never
@@ -27,30 +30,24 @@ init =
 
 type Msg 
     = GetQuote
-    | FetchSucceed String
-    | FetchFail Http.Error
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update action model =
-    case action of
+update msg model =
+    case msg of
         GetQuote ->
-            (model, getRandomQuote)
-            
-        FetchSucceed newQuote ->
-            (Model newQuote, Cmd.none)
-            
-        FetchFail _ ->
             (model, Cmd.none)
+            
             
 -- View
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick GetQuote ] [ text "Get a Quote" ]
-        , div [] [ text (model.quote) ]
-        ]
-        
+    div [ class "jumbotron row text-center" ] [
+        h2 [] [ text "Chuck Norris Quoter" ]
+        , button [ class "btn btn-primary", onClick GetQuote ] [ text "Get a Quote" ]
+        , blockquote [ ] [ text (model.quote) ]
+    ]
+    
 -- Subscriptions
 
 subscriptions : Model -> Sub Msg
